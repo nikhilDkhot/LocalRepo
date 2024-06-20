b23506c2f8b2d662cbdc492c7bb7461c53a7c8d3
nikhilDkhot <nikhillkhot369@gmail.com>
2024-06-20 10:59:02 +0530

    this is  map function practice

--- map1.js
+++ map1.js
@@ -1,5 +1,48 @@
-let arr = [45,88,94]
+/*let arr = [45,88,94]
 arr.map((value)=>{
     console.log(value)
 })
-console.log(arr)
+console.log(arr)*/
+
+/* let arr = [45,88,94]
+arr.map((value)=>{
+    console.log(value)
+    return value + 1
+})
+console.log(arr) */  // here there is no change in main arr as map funtion create new arr
+
+/* let arr = [45,88,94]
+let arr1 = arr.map((value)=>{
+    console.log(value)
+    return value + 1
+})
+console.log(arr1) 
+console.log(arr) */ 
+
+/* let arr = [45,88,94]
+let arr1 = arr.map((value,index,array)=>{
+    console.log(value,index,array)
+    //console.log(index)
+    return value + 1
+})
+console.log(arr1) 
+console.log(arr) */
+/* 
+let arr = [45,88,94]
+let arr1 = arr.map((value,index,array)=>{
+    console.log(value,index,array)
+    //console.log(index)
+    return value + index
+})
+console.log(arr1) 
+console.log(arr) */
+
+
+
+
+
+
+
+
+
+
