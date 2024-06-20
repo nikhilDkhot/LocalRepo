41602528e2c076a055857e1419cd8e9979703603
nikhilDkhot <nikhillkhot369@gmail.com>
2024-06-20 11:14:34 +0530

    this is  filter function

---
+++ filter.js
@@ -0,0 +1,24 @@
+/* let arr = [12,45,78,56,84,55,21,33]
+
+arr.filter((value)=>{
+
+    if(value%2==0){
+        console.log(value)
+    }
+})
+
+console.log(arr) */
+
+let arr = [12,45,78,56,84,55,21,33]
+
+let arr1 = arr.filter((value)=>{
+
+    if(value%2==0){
+        console.log(value)
+        return value
+    }
+    
+})
+console.log(arr1)
+
+console.log(arr)
