d5d588e4f23153c3cc103b1c7adee51b0ac97bc9
nikhilDkhot <nikhillkhot369@gmail.com>
2024-06-20 12:19:48 +0530

    this is  forEach function

---
+++ foreach.js
@@ -0,0 +1,19 @@
+/* let num = [8,4,7,3,5,1]
+
+num.forEach((value)=>
+{
+    console.log(value)
+}) */
+
+
+/* 
+let num = [8,4,7,3,5,1]
+
+let num1 = num.forEach((value)=>  //forEach does not return a new array
+{
+    console.log(value*2)
+    //return value * 2
+})
+
+console.log(num1)
+console.log(num) */
